e9c3217c613d9f1a6e914a5792238195f270f1e5
bogdan <bogdanp28@gmail.com>
2024-08-27 00:00:48 +0300

    updated test for through model

--- playlists/tests.py
+++ playlists/tests.py
@@ -1,3 +1,4 @@
+from django.db.models.manager import BaseManager
 from django.test import TestCase
 from .models import Playlist, PublishStateOptions
 from django.utils import timezone #type: ignore
@@ -8,24 +9,26 @@ from videos.models import Video
 # Create your tests here.
 class PlaylistModelTestCase(TestCase):
     def create_videos(self):
-        video_a = Video.objects.create(title="Blade Runner original", video_id="abc123")
-        video_b = Video.objects.create(title="Blade Runner 2049", video_id="abc124")
-        video_c = Video.objects.create(title="Blade Runner 2099", video_id="abc125")
-        self.video_a = video_a
-        self.video_b = video_b
-        self.video_c = video_c
+        video_a: Video = Video.objects.create(title="Blade Runner original", video_id="abc123")
+        video_b: Video = Video.objects.create(title="Blade Runner 2049", video_id="abc124")
+        video_c: Video = Video.objects.create(title="Blade Runner 2099", video_id="abc125")
+        self.video_a: Video = video_a
+        self.video_b: Video = video_b
+        self.video_c: Video = video_c
+        self.video_qs: BaseManager[Video] = Video.objects.all()
         
     def setUp(self):
         self.create_videos()
-        self.obj_a = Playlist.objects.create(title="Test Title", video=self.video_a)
-        obj_b = Playlist.objects.create(title="This is my title", 
+        self.obj_a: Playlist = Playlist.objects.create(title="Test Title", video=self.video_a)
+        obj_b: Playlist = Playlist.objects.create(title="This is my title", 
                                              state=PublishStateOptions.PUBLISH,
                                              video=self.video_a)
+        
         # obj_b.videos.set([self.video_a, self.video_b, self.video_c])
-        v_qs = Video.objects.all()
+        v_qs: BaseManager[Video] = self.video_qs
         obj_b.videos.set(v_qs)
         obj_b.save()
-        self.obj_b = obj_b
+        self.obj_b: Playlist = obj_b
         
     def test_video_playlist(self):
         qs = self.video_a.playlist_featured.all() #type: ignore
@@ -34,6 +37,12 @@ class PlaylistModelTestCase(TestCase):
     def test_playlist_video_items(self):
         count = self.obj_b.videos.all().count()
         self.assertEqual(count, 3)
+        
+    def test_playlist_video_through_model(self):
+        video_qs = sorted(list(self.video_qs.values_list("id"))) # type: ignore
+        playlist_obj_video_qs = sorted(list(self.obj_b.videos.all().values_list("id"))) #type: ignore
+        playlist_obj_playlist_item_qs = sorted(list(self.obj_b.playlistitem_set.all().values_list("video"))) #type: ignore
+        self.assertEqual(video_qs, playlist_obj_video_qs, playlist_obj_playlist_item_qs)
         
     def test_video_playlist_ids_propery(self):
         ids = self.obj_a.video.get_playlist_ids() #type: ignore
@@ -46,7 +55,7 @@ class PlaylistModelTestCase(TestCase):
         self.assertEqual(test_slug, self.obj_a.slug)
     
     def test_valid_title(self):
-        title = "Test Title"
+        title = "This is my title"
         qs = Playlist.objects.filter(title=title)
         self.assertTrue(qs.exists())
         
